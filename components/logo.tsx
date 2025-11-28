7ce42febdf30f50cb940d1b03873defd3c2fe6ea
Rahul Meena <rahul.meena@indx.ai>
2025-11-28 22:16:56 +0530

    init commit and auth done

---
+++ components/logo.tsx
@@ -0,0 +1,11 @@
+import { Wallet } from "lucide-react";
+
+function Logo() {
+    return (
+    <div className="flex gap-x-2">
+        <Wallet /> Fintra
+    </div>
+    );
+}
+
+export default Logo;
